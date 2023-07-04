39c3975e449d24d07fb45a13cf30522077f834ec
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-07-04 20:12:17 +0900

    회원가입 파일명 번경, 로그인 기능 작업시작

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import {} from "react-dom";
 import { Route, Routes } from "react-router-dom";
 import { GlobalStyle } from "./style";
 import { Main } from "./pages";
-import { Sign } from "./pages/login";
+import { SignUp } from "./pages/signup";
+import { SignIn } from "./pages/signin";
+
 
 function App() {
   return (
     <GlobalStyle>
       <Routes>
         <Route path="/" element={<Main />}></Route>
-        <Route path="/auth/signin" element={<Sign />}></Route>
+        <Route path="/auth/signup" element={<SignUp />}></Route>
+        <Route path="/auth/signin" element={<SignIn />}></Route>
       </Routes>
     </GlobalStyle>
   );
