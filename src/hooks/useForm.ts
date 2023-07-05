5c579332508bc98b3f6e94cdddac89c64f061696
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-07-05 10:44:31 +0900

    useform 이미지 입력
    확인 부탁드립니다!

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -4,10 +4,18 @@ export const useForm = <T>(initial: T) => {
   const [state, setState] = useState<T>(initial);
 
   const onHandleChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
   ) => {
-    const { value, name } = e.target;
-    setState({ ...state, [name]: value });
+    const { value, name , type} = e.target;
+
+    if (type === "file"){
+      const file = (e.currentTarget as HTMLInputElement | null)?.files?.[0] || null;
+      setState({ ...state, [name]: value });
+    }
+    else {
+      setState({ ...state, [name]: value });
+
+    }
   };
   return { state, onHandleChange, setState };
 };
