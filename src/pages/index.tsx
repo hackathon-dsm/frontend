5e46a985fd4e1b59e7b7075ecf2381d60229cffa
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-07-05 14:41:10 +0900

    버튼, 하단 줄 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,6 +56,10 @@ export const Main = () => {
       <_LogoWrapper>
         <Taxi /> Texcer
       </_LogoWrapper>
+      <_LinkButtonWrapper>
+        <Button>택시콜</Button>
+        <Button>마이페이지</Button>
+      </_LinkButtonWrapper>
       </Container>
       
       <_Wrapper>
@@ -131,15 +135,39 @@ const _LogoWrapper = styled.div`
   gap: 6px;
   font-size: 50px;
   font-weight: 700;
+ 
 
 `;
 const Container = styled.div`
   height: 50px;
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
   margin: 50px 20px 0 50px;
   gap: 60px;
-  
+  border-bottom: 2px solid #ffd15c;
+`;
+
+const Button = styled.button<{ width?: string; isSubmit?: boolean }>`
+  padding: 10px;
+  width: 200px;
+  height: 64px;
+  background-color: #ffd15c;
+  color: white;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  margin-bottom: 10px;
+  font-size: 17px;
+  font-weight: 500;
+  margin-left: 20px;
+`;
+
+const _LinkButtonWrapper = styled.div`
+  display: inline-;
+  flex-direction: row;
+  align-items: center;
+  gap: 25px;
+
 `;
